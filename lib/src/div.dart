434b8924c445aa494532cc3c6988c25b8155471e
Ben Landrette <128928536+BendyLand@users.noreply.github.com>
2026-02-01 22:33:11 -0500

    Remove unneccessary doc comments

--- lib/src/div.dart
+++ lib/src/div.dart
@@ -12,13 +12,11 @@ class Div extends Element with IdInfo, ClassInfo {
   @override
   String get str => '<div$idStr$classes>$content</div>';
 
-  /// A list of `Element`s that are children of the current Element.
   final List<Element> children;
 
-  /// Creates a Div object with optional [id], [classList], and [children].
   const Div({this.id, this.children = const [], this.classList = const []});
 
-  /// Constructs a valid HTML string out of the elements from `this.children`.
   // FIX: Update to use StringBuilder for efficiency
+  /// Constructs a valid HTML string out of the elements from `this.children`.
   String get content => children.map((e) => e.str).join();
 }
